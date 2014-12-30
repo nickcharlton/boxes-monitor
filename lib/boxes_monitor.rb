ffe7bdfa8dc47736558132e007379adeca1e8a98
Nick Charlton <hello@nickcharlton.net>
2014-12-30 17:06:27 +0000

    Adds a method to load in the dependencies file.
    This is independent from both the Sinatra frontend and the Rakefile so it can be
    used in either.

--- lib/boxes_monitor.rb
+++ lib/boxes_monitor.rb
@@ -1,1 +1,8 @@
 require 'boxes_monitor/version'
+require 'boxes_monitor/version_scraper.rb'
+
+module BoxesMonitor
+  def self.dependencies
+    YAML.load_file File.expand_path('../../dependencies.yml', __FILE__)
+  end
+end
