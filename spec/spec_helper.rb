401a712453032cc11753d8b416e48b51ed974466
Nick Charlton <nick@nickcharlton.net>
2015-01-25 18:36:53 +0000

    Adds a Sinatra friendly spec_helper.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,38 @@
+ENV['RACK_ENV'] ||= 'test'
+
+require 'rspec'
+
+# load the application
+require File.expand_path('../../app', __FILE__)
+require 'rack/test'
+
+set :environment, :test
+set :run, false
+set :raise_errors, true
+set :logging, false
+
+def app
+  Sinatra::Application
+end
+
+# load in the support directory
+Dir['support/**/*.rb'].each { |f| require File.expand_path f, __dir__ }
+
+# bring up the migrations
+ActiveRecord::Migrator.up 'db/migrate'
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  # specs should run randomly.
+  config.order = :random
+  Kernel.srand config.seed
+end
